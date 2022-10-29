bbb8960cc7e9e3d6a048fb1a4041136a9b80c7d0
OttoNyongole <nyongole.otto@gmail.com>
2022-10-29 20:14:04 +0300

    updated console file

--- console.py
+++ console.py
@@ -1,21 +1,39 @@
-#!/usr/bin/pythob3
+#!/usr/bin/python3
 """Condole Module command interpreter"""
 
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-
-     """method to quit the command line interpreter"""
-    def do_quit(self, arg):
-        """quit the command prompt"""
+     """method to quit the command line interpreter
+     """
+     def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """
-        EOF signal to quit the console.
-        """
+        """EOF signal to exit the program"""
         print("")
         return True
+
+    def emptyline(self):
+        """Do nothing upon receiving empty line"""
+        pass
+
+    def do_create(self, arg):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        argl = shlex.split(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argl[0])().id)
+            storage.save()
+
+
+
 
 if __name__=="__main__":
     HBNBCommand().cmdloop
