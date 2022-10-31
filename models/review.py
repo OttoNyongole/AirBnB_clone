bef30f1f922cc06b042624c51e3e80d770c94465
AngelahMbula <mbula.angelah@gmail.com>
2022-10-31 14:42:09 +0300

    Initial commit

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines Review class."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Inherits rom BaseModel represents a review."""
+    place_id = ""
+    user_id = ""
+    text = ""
